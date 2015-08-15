187c973af8424a83de8512fc1d21cb04aa1cbd82
s10196 <s10196@ubuntu.com>
2015-08-15 20:15:48 +0800

    add java test

---
+++ java/learn/src/learn/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package learn.test;
+
+/**
+ * @author s10196
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
